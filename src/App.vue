a55e673a2542c198d0b5911d48f79e526cde6caa
emascapa <emascapa@gmail.com>
2022-05-03 16:40:00 +0200

    Initial commit with header

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <SiteHeader/>
+    <SiteMain/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+/* import HelloWorld from './components/HelloWorld.vue' */
+import SiteHeader from '@/components/HeaderComponent.vue'
+import SiteMain from '@/components/MainComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SiteHeader,
+    SiteMain,
+  }
+}
+</script>
+
+<style lang="scss">
+
+@import '@/assets/sass/style.scss';
+
+/* #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="container">
+    <h3>--> content here</h3>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+    .container {
+        background-color: black;
+        padding: 2rem 0;
+        
+        h3 {
+            color: white;
+        }
+    }
+
+</style>
